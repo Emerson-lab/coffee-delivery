8c1a44da4db0a445d168f06732c7bb9725da7aca
Emerson Trindade <trinddade29@gmail.com>
2023-10-28 17:23:10 -0300

    chore: config fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/theme/default";
 
 function App() {
 
   return (
-    <h1>app</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>app</h1>
+    </ThemeProvider>
   )
 }
 
